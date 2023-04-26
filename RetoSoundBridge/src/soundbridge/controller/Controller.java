50348eb67c6f4bded0792ee36a946c58feac44a0
in1dw3 <in1dw3@HZ352033.elorrieta.int>
2023-04-26 18:03:42 +0200

    client's login validation

--- RetoSoundBridge/src/soundbridge/controller/Controller.java
+++ RetoSoundBridge/src/soundbridge/controller/Controller.java
@@ -1,5 +1,68 @@
 package soundbridge.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import soundbridge.database.utils.DBUtils;
+
 public class Controller {
 
+	public boolean comprobarCliente(String userLogin, String passwdLogin) {
+
+		String sql = "select * from cliente where personalId=? and passwd=?";
+
+		Connection connection = null;
+
+		ResultSet resultSet = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+
+			Class.forName(DBUtils.DRIVER);
+
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, personalId);
+			preparedStatement.setString(2, passwd);
+			resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+
+				return true;
+
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+
+			}
+			;
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+
+			}
+			;
+		}
+		return false;
+	}
 }
--- RetoSoundBridge/src/soundbridge/view/Menu.java
+++ RetoSoundBridge/src/soundbridge/view/Menu.java
@@ -3,26 +3,15 @@ package soundbridge.view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 
 public class Menu {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Menu window = new Menu();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	public JFrame frame;
+	private JTextField textFieldUserLogIn;
+	private JPasswordField passwordFieldLogIn;
 
 	/**
 	 * Create the application.
@@ -36,8 +25,27 @@ public class Menu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1000, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.getContentPane().setLayout(null);
+		
+		textFieldUserLogIn = new JTextField();
+		textFieldUserLogIn.setBounds(45, 80, 86, 20);
+		frame.getContentPane().add(textFieldUserLogIn);
+		textFieldUserLogIn.setColumns(10);
+		
+		JLabel LabelUserLogIn = new JLabel("USUARIO");
+		LabelUserLogIn.setBounds(45, 50, 70, 20);
+		frame.getContentPane().add(LabelUserLogIn);
+		
+		JLabel LabelPasswdLogIn = new JLabel("PASSWORD");
+		LabelPasswdLogIn.setBounds(45, 127, 70, 20);
+		frame.getContentPane().add(LabelPasswdLogIn);
+		
+		passwordFieldLogIn = new JPasswordField();
+		passwordFieldLogIn.setBounds(45, 158, 86, 20);
+		frame.getContentPane().add(passwordFieldLogIn);
 	}
-
 }
