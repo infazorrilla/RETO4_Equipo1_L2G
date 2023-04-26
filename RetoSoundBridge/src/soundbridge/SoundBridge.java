3d268510edd210fa3539acac58acb3527edf631c
Leire Lasa <leire.lasa.amo@gmail.com>
2023-04-26 17:22:35 +0200

    add code to launcher

--- RetoSoundBridge/src/soundbridge/SoundBridge.java
+++ RetoSoundBridge/src/soundbridge/SoundBridge.java
@@ -1,5 +1,21 @@
 package soundbridge;
 
+import java.awt.EventQueue;
+
+import soundbridge.view.Menu;
+
 public class SoundBridge {
 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu window = new Menu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
