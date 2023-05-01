52e30e245372040cf640afcd57e2d4b6c8dcb72d
Gonzalo <gonzalo.barrasaal@elorrieta-errekamari.com>
2023-05-01 17:59:59 +0200

    bloquear clientes

--- RetoSoundBridge/src/soundbridge/view/panels/EmployeeManagesClients.java
+++ RetoSoundBridge/src/soundbridge/view/panels/EmployeeManagesClients.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
 
@@ -58,10 +59,10 @@ public class EmployeeManagesClients extends JPanel {
 						String nombre= client.getName();
 						String apellido=client.getLastName();
 						String genero=client.getGender();
-						String DNI =client.getPersonalId();
+						String dNI =client.getPersonalId();
 						String username=client.getUsername();
 						String contrasena=client.getPasswd();
-						modelClients.addRow(new String[] { nombre, apellido, genero, DNI, username, contrasena });
+						modelClients.addRow(new String[] { nombre, apellido, genero, dNI, username, contrasena });
 					}
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
@@ -76,19 +77,43 @@ public class EmployeeManagesClients extends JPanel {
 		add(btnSelectAllClients);
 		
 		JScrollPane scrollPaneResumenCompra = new JScrollPane();
-		scrollPaneResumenCompra.setBounds(44, 179, 904, 455);
+		scrollPaneResumenCompra.setBounds(44, 179, 904, 383);
 		add(scrollPaneResumenCompra);
 
 		tableClients = new JTable();
+		tableClients.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		tableClients.setDefaultEditor(Object.class, null);
 		scrollPaneResumenCompra.setViewportView(tableClients);
 		tableClients.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		scrollPaneResumenCompra.setViewportView(tableClients);
-		Object[] columnsManagesClients = { "Nombre", "Apellido", "Género", "DNI", "Suscripcion", "Username", "Contraseña"  };
+		Object[] columnsManagesClients = { "Nombre", "Apellido", "Género", "DNI", "Username", "Contraseña"  };
 
 		 modelClients= new DefaultTableModel();
 		modelClients.setColumnIdentifiers(columnsManagesClients);
 		tableClients.setModel(modelClients);
+		
+		JButton btnBloqClient = new JButton("Bloquear cliente");
+		btnBloqClient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClientManager clientmanager = new ClientManager();
+				Client client = new Client();
+				String username = (String) modelClients.getValueAt(tableClients.getSelectedRow(), 4);
+				client.setUsername(username);
+				try {
+					Client selectedClient =clientmanager.getClientByUsername(username);
+					selectedClient.setBlocked(true);
+					clientmanager.update(selectedClient);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnBloqClient.setBounds(139, 600, 126, 23);
+		add(btnBloqClient);
 		
 	}
 }
